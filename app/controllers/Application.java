85c0be205e02ca2ca4edfc54b1206bf43e29f38e
f.leporcq <f.leporcq@wyniwyg.com>
2012-01-03 10:33:15 +0100

    Ajout de commentaire et test de commit sur master

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,7 +7,10 @@ public class Application extends Controller {
 
     public static void index() {
 
+        // Création d'une grille
         Grid grid = new Grid(15, 11);
+
+        // Génération pseudo aléatoire des cases noires
         grid.generateRandomBlackCells(20, 2);
 
         render(grid);
